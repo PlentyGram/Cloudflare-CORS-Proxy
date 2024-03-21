b47fdf620baff7b66286468475c4e0b7a8f55758
https://www.plentygram.com <support@plentygram.com>
2024-03-21 03:09:02 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,192 +1,40 @@
-/*
- * https://github.com/PlentyGram/Cloudflare-CORS-Proxy
- *
- * 2019-10-12 - 2022-06-28
- * plentygram.com
- */
-
-addEventListener('fetch', event => {
-    event.passThroughOnException()
-    event.respondWith(handleRequest(event))
+// forked from: https://github.com/chebyrash/cors
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request))
 })
 
-/**
-* Configurations
-*/
-const config = {
-    // 从 https://www.plentygram.com/ 申请并修改令牌
-    plentygramToken: "00000000-0000-0000-0000-000000000000",
-    // 是否丢弃请求中的 Referer，在目标网站应用防盗链时有用
-    dropReferer: false
-};
+async function handleRequest(request) {
+  try {
+    const url = new URL(request.url);
 
-/**
-* Respond to the request
-* @param {Request} request
-*/
-async function handleRequest(event) {
-    const { request } = event;
-
-    //请求头部、返回对象
-    let reqHeaders = new Headers(request.headers),
-        outBody, outStatus = 200, outStatusText = 'OK', outCt = null, outHeaders = new Headers({
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers": reqHeaders.get('Access-Control-Allow-Headers') || "Accept, Authorization, Cache-Control, Content-Type, DNT, If-Modified-Since, Keep-Alive, Origin, User-Agent, X-Requested-With, Token, x-access-token"
-        });
-
-    try {
-        //取域名第一个斜杠后的所有信息为代理链接
-        let url = request.url.substr(8);
-        url = decodeURIComponent(url.substr(url.indexOf('/') + 1));
-
-        //需要忽略的代理
-        if (request.method == "OPTIONS" || url.length < 3 || url.indexOf('.') == -1 || url == "favicon.ico" || url == "robots.txt") {
-            //输出提示
-            const invalid = !(request.method == "OPTIONS" || url.length === 0)
-            outBody = JSON.stringify({
-                code: invalid ? 400 : 0,
-                usage: 'Host/{URL}',
-                source: 'https://github.com/PlentyGram/Cloudflare-CORS-Proxy',
-                note: 'Blocking a large number of requests, please deploy it yourself'
-            });
-            outCt = "application/json";
-            outStatus = invalid ? 400 : 200;
-        }
-        else {
-            url = fixUrl(url);
-
-            //构建 fetch 参数
-            let fp = {
-                method: request.method,
-                headers: {}
-            }
-
-            //保留头部其它信息
-            const dropHeaders = ['content-length', 'content-type', 'host'];
-            if (config.dropReferer) dropHeaders.push('referer');
-            let he = reqHeaders.entries();
-            for (let h of he) {
-                const key = h[0], value = h[1];
-                if (!dropHeaders.includes(key)) {
-                    fp.headers[key] = value;
-                }
-            }
-
-            // 是否带 body
-            if (["POST", "PUT", "PATCH", "DELETE"].indexOf(request.method) >= 0) {
-                const ct = (reqHeaders.get('content-type') || "").toLowerCase();
-                if (ct.includes('application/json')) {
-                    fp.body = JSON.stringify(await request.json());
-                } else if (ct.includes('application/text') || ct.includes('text/html')) {
-                    fp.body = await request.text();
-                } else if (ct.includes('form')) {
-                    fp.body = await request.formData();
-                } else {
-                    fp.body = await request.blob();
-                }
-            }
-
-            // 发起 fetch
-            let fr = (await fetch(url, fp));
-            outCt = fr.headers.get('content-type');
-            outStatus = fr.status;
-            outStatusText = fr.statusText;
-            outBody = fr.body;
-        }
-    } catch (err) {
-        outCt = "application/json";
-        outBody = JSON.stringify({
-            code: -1,
-            msg: JSON.stringify(err.stack) || err
-        });
-        outStatus = 500;
+    if (url.pathname === "/") {
+      return new Response(`{"usage": "${url.origin}/<url>"}`);
     }
 
-    //设置类型
-    if (outCt && outCt != "") {
-        outHeaders.set("content-type", outCt);
+    function addHeaders(response) {
+      response.headers.set("Access-Control-Allow-Origin", "*");
+      response.headers.set("Access-Control-Allow-Credentials", "true");
+      response.headers.set("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+      response.headers.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    }
+    
+    let response;
+    if (request.method == "OPTIONS") {
+      response = new Response("");
+      addHeaders(response);
+      return response;
     }
 
-    let response = new Response(outBody, {
-        status: outStatus,
-        statusText: outStatusText,
-        headers: outHeaders
-    })
-
-    //日志接口
-    if (config.plentygramToken != "00000000-0000-0000-0000-000000000000") {
-        plentygram.add(event, request, response);
-    }
-
+    response = await fetch(request.url.slice(url.origin.length + 1), {
+        method: request.method,
+        headers: request.headers,
+        redirect: "follow",
+        body: request.body
+    });
+    response = new Response(response.body, response)
+    addHeaders(response);
     return response;
-
-    // return new Response('OK', { status: 200 })
+  } catch (e) {
+    return new Response(e.stack || e, {status: 500});
+  }
 }
-
-// 补齐 url
-function fixUrl(url) {
-    if (url.includes("://")) {
-        return url;
-    } else if (url.includes(':/')) {
-        return url.replace(':/', '://');
-    } else {
-        return "http://" + url;
-    }
-}
-
-/**
-* 日志
-*/
-const plentygram = {
-
-    /**
-     * 构建发送主体
-     * @param {any} request
-     * @param {any} response
-     */
-    buildBody: (request, response) => {
-        const hua = request.headers.get("user-agent")
-        const hip = request.headers.get("cf-connecting-ip")
-        const hrf = request.headers.get("referer")
-        const url = new URL(request.url)
-
-        const body = {
-            method: request.method,
-            statusCode: response.status,
-            clientIp: hip,
-            referer: hrf,
-            userAgent: hua,
-            host: url.host,
-            path: url.pathname,
-            proxyHost: null,
-        }
-
-        if (body.path.includes(".") && body.path != "/" && !body.path.includes("favicon.ico")) {
-            try {
-                let purl = fixUrl(decodeURIComponent(body.path.substring(1)));
-
-                body.path = purl;
-                body.proxyHost = new URL(purl).host;
-            } catch { }
-        }
-
-        return {
-            method: "POST",
-            body: JSON.stringify(body)
-        }
-    },
-
-    /**
-     * 添加
-     * @param {any} event
-     * @param {any} request
-     * @param {any} response
-     */
-    add: (event, request, response) => {
-        let url = `https://logsene-receiver.plentygram.com/${config.plentygramToken}/example/`;
-        const body = plentygram.buildBody(request, response);
-
-        event.waitUntil(fetch(url, body))
-    }
-};
